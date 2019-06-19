7795a4e125a75fd3aa0dcf5c93df7b04ec6afa4e
Adina Johnson <adina.johnson@appfolio.com>
2019-06-19 15:52:03 -0700

    aj - add create action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,4 +3,18 @@ class ImagesController < ActionController::Base
     @image = Image.new
   end
 
+  def create
+    @image = Image.create(image_params)
+    if @image.save
+      redirect_to image_path(@image), notice: 'Image was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
+  end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -7,4 +7,22 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_select '#header', 'Enter an image url'
   end
 
+  def test_create__succeed
+    assert_difference('Image.count', 1) do
+      image_params = { url: 'https://pbs.twimg.com/profile_images/962170088941019136/lgpCD8X4_400x400.jpg' }
+      post images_path, params: { image: image_params }
+    end
+
+    assert_redirected_to image_path(Image.last)
+    assert_equal 'Image was successfully created.', flash[:notice]
+  end
+
+  def test_create__fail
+    assert_no_difference('Image.count') do
+      image_params = { url: 'es/962170088941019136/lgpCD8X4_400' }
+      post images_path, params: { image: image_params }
+    end
+
+    assert_response :unprocessable_entity
+  end
 end
