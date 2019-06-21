bda2568d7d11f8dc0a3241fc3ab68dbcc9aa2102
Adina Johnson <adina.johnson@appfolio.com>
2019-06-21 11:26:49 -0700

    aj - add 20 initial images to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# rubocop:disable LineLength
+Image.create!([
+  { url: 'https://pbs.twimg.com/profile_images/962170088941019136/lgpCD8X4_400x400.jpg' },
+  { url: 'https://static.wixstatic.com/media/38caab_553760a4c5474bb7a517357fbafd3e48.jpg' },
+  { url: 'http://akc.org/wp-content/uploads/2015/09/Siberian-Husky-Two-Puppies.jpg' },
+  { url: 'https://www.akc.org/wp-content/themes/akc/component-library/assets/img/welcome.jpg' },
+  { url: 'https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/09/dog-landing-hero-lg.jpg' },
+  { url: 'https://www.readersdigest.ca/wp-content/uploads/sites/14/2017/03/large-breed-dog-benefits-patience.jpg' },
+  { url: 'https://i0.wp.com/www.simplypets.com/wp-content/uploads/2019/05/Fluffy-Dog.jpg' },
+  { url: 'https://s.abcnews.com/images/GMA/Lab_hpMain_12x5_992.jpg' },
+  { url: 'https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/30/2560x1280/landscape-1500925839-golden-retriever-puppy.jpg' },
+  { url: 'https://d17fnq9dkz9hgj.cloudfront.net/uploads/2018/04/Pomeranian_02.jpg' },
+  { url: 'https://fortunedotcom.files.wordpress.com/2019/01/boo.jpg' },
+  { url: 'https://hips.hearstapps.com/wdy.h-cdn.co/assets/17/39/1506709524-cola-0247.jpg' },
+  { url: 'https://image.shutterstock.com/image-photo/adorable-happy-puppy-caught-motion-260nw-696648019.jpg' },
+  { url: 'https://cdn6.littlethings.com/app/uploads/2017/05/cute-dog-names-1200.jpg' },
+  { url: 'https://cdn.images.express.co.uk/img/dynamic/128/590x/Dog-news-cute-pictures-bunny-rabbit-Polish-Tatra-Sheepdog-973655.jpg' },
+  { url: 'https://topdogtips.com/wp-content/uploads/2014/12/Chow-Chow-puppy-is-part-of-top-10-cute-dog-breeds-list.jpg' },
+  { url: 'https://3milliondogs.com/blog-assets-two/2016/07/Screen-Shot-2016-07-27-at-8.55.40-AM-600x400.png' },
+  { url: 'https://www.thoughtco.com/thmb/oyyzjRX_yrvI-YpwmgX_j1lo9e0=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-166270886-56e472f43df78c5ba0571b9a.jpg' },
+  { url: 'https://www.readersdigest.ca/wp-content/uploads/sites/14/2018/10/Simple-Ways-to-Tell-If-Your-Dog-Is-Smart-1.jpg' },
+  { url: 'https://i1.wp.com/www.eventstodayz.com/wp-content/uploads/2016/12/white-cute-dog-images-2017.jpg' }
+])
+# rubocop:enable LineLength
