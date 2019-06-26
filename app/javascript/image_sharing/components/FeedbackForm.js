3de92c77fbb37faf063ba97dee1aec0859b6c39d
Adina Johnson <adina.johnson@appfolio.com>
2019-06-25 17:07:49 -0700

    aj - feedback form sends data to be saved

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -1,20 +1,53 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Alert } from 'reactstrap';
+import { observer } from 'mobx-react';
+import { action, observable } from 'mobx';
 
+import { post } from '../utils/helper';
+
+@observer
 export default class FeedbackForm extends React.Component {
+  @observable name = '';
+  @observable comments = '';
+  @observable response = undefined;
+  @observable responseColor = undefined;
+
+  @action setName = name => this.name = name;
+  @action setComments = comments => this.comments = comments;
+
+  click = () => post('/api/feedbacks', {
+    name: this.name,
+    comments: this.comments
+  }).then((value) => {
+    this.response = value.message;
+    this.responseColor = 'success';
+    this.name = ''; this.comments = '';
+  }).catch((error) => {
+    this.response = error.data.message;
+    this.responseColor = 'danger';
+  })
+
   render() {
     return (
-      <Form>
-        <FormGroup>
-          <Label for="name">Your name:</Label>
-          <Input type="name" name="name" id="name" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="comments">Comments:</Label>
-          <Input type="comments" name="comments" id="comments" />
-        </FormGroup>
-        <Button>Submit</Button>
-      </Form>
+      <div>
+        { this.response && <Alert color={this.responseColor}>{this.response}</Alert> }
+        <Form>
+          <FormGroup>
+            <Label for="name">Your name:</Label>
+            <Input type="name" name="name" id="name" value={this.name} onChange={(e) => { this.setName(e.target.value); }} />
+          </FormGroup>
+          <FormGroup>
+            <Label for="comments">Comments:</Label>
+            <Input type="comments" name="comments" id="comments" value={this.comments} onChange={(e) => { this.setComments(e.target.value); }} />
+          </FormGroup>
+          <Button
+            color="primary"
+            onClick={this.click}
+          >
+            Submit
+          </Button>
+        </Form>
+      </div>
     );
   }
 }
--- app/javascript/image_sharing/test/components/Form.spec.js
+++ app/javascript/image_sharing/test/components/Form.spec.js
@@ -2,9 +2,11 @@
 
 import assert from 'assert';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import React from 'react';
-import { FormGroup, Label, Input } from 'reactstrap';
+import { FormGroup, Label, Input, Button, Alert } from 'reactstrap';
 
+import * as api from '../../utils/helper';
 import FeedbackForm from '../../components/FeedbackForm';
 
 describe('<FeedbackForm />', () => {
@@ -18,5 +20,75 @@ describe('<FeedbackForm />', () => {
 
     const inputs = form.find(Input);
     assert.strictEqual(inputs.length, 2);
+  });
+
+  it('should reset the form after submission', async () => {
+    const wrapper = shallow(<FeedbackForm />);
+    const inputs = wrapper.find(Input);
+    const stub = sinon.stub(api, 'post').resolves({ message: 'test' });
+
+    inputs.at(0).simulate('change', { target: { value: 'fake name' } });
+    inputs.at(1).simulate('change', { target: { value: 'fake comments' } });
+
+    const button = wrapper.find(Button);
+
+    await button.simulate('click');
+
+    assert.strictEqual(inputs.at(0).props().value, '');
+    assert.strictEqual(inputs.at(1).props().value, '');
+
+    stub.restore();
+  });
+
+  it('should have the correct information in the post request', async () => {
+    const wrapper = shallow(<FeedbackForm />);
+    const inputs = wrapper.find(Input);
+    const stub = sinon.stub(api, 'post').resolves({ message: 'test' });
+
+    inputs.at(0).simulate('change', { target: { value: 'fake name' } });
+    inputs.at(1).simulate('change', { target: { value: 'fake comments' } });
+
+    const button = wrapper.find(Button);
+
+    await button.simulate('click');
+
+    assert(stub.calledOnceWithExactly('/api/feedbacks', { name: 'fake name', comments: 'fake comments' }));
+    stub.restore();
+  });
+
+  it('should render the correct response on success', async () => {
+    const wrapper = shallow(<FeedbackForm />);
+    const inputs = wrapper.find(Input);
+    const stub = sinon.stub(api, 'post').resolves({ message: 'GOOD SUBMISSION' });
+
+    inputs.at(0).simulate('change', { target: { value: 'fake name' } });
+    inputs.at(1).simulate('change', { target: { value: 'fake comments' } });
+
+    const button = wrapper.find(Button);
+
+    await button.simulate('click');
+
+    assert.strictEqual(wrapper.find(Alert).children().text(), 'GOOD SUBMISSION');
+
+    stub.restore();
+  });
+
+  it('should render the correct response on failure', async () => {
+    const wrapper = shallow(<FeedbackForm />);
+    const inputs = wrapper.find(Input);
+    const stub = sinon.stub(api, 'post').rejects({ data: { message: 'BAD SUBMISSION' } });
+
+    inputs.at(0).simulate('change', { target: { value: 'fake name' } });
+    inputs.at(1).simulate('change', { target: { value: 'fake comments' } });
+
+    const button = wrapper.find(Button);
+
+    const promise = button.prop('onClick')();
+    await promise;
+    wrapper.update();
+
+    assert.strictEqual(wrapper.find(Alert).children().text(), 'BAD SUBMISSION');
+
+    stub.restore();
   });
 });
